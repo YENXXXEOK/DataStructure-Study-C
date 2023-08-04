cf60bc2743e3ab2a0136b5a5f50958972551cac1
YEONXX <yskim4085@naver.com>
2023-08-04 16:58:49 +0900

    자료구조 샘플 커밋

---
+++ Study/SingleLinkedList/Structure.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef struct NODE
+{
+	int nData;
+	struct NODE* next;
+}NODE;
+
+main()
+{
+	NODE list[5] = { 0, };
+
+	for (int i = 0; i < 4; i++)
+	{
+		list[i].next = &list[i + 1];
+		list[i].nData = (i + 1) * 100;
+	}
+	list[4].next = NULL;
+	list[4].nData = 500;
+
+	for (int i = 0; i < 5; i++)
+		printf("[%d] %d\n",i,  list[i].nData);
+
+	NODE* pHead = &list[0];
+	for(pHead; pHead != NULL; pHead = pHead->next)
+		printf("[0x%p] %d\n", pHead, pHead->nData);
+}
